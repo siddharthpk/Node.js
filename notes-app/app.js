c8f62e2f12022667032abcc55b79e65cdab615d9
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-03-26 17:09:26 -0700

    working with npm module 'chalk'

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,12 @@
-const validator = require('validator')
+//const validator = require('validator')
 
-console.log(validator.isEmail('siddharthpk@gmail.com'))
+//console.log(validator.isEmail('siddharthpk@gmail.com'))
 
-console.log(validator.isURL('https://siddharthpk.me'))
+//console.log(validator.isURL('https://siddharthpk.me'))
+
+const chalk = require('chalk')
+
+console.log(chalk.green('Success'))
+console.log(chalk.bold.green('Success'))
+console.log(chalk.bold.red('Success'))
+console.log(chalk.bgRed('Success'))
