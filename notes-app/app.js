533f88ca89683a4cd40459c47ec7ca8d2879c855
Siddharth Pathak <siddharthpathak@uvic.ca>
2021-03-28 01:52:28 -0700

    yargs package and learnt about creating command

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,19 +2,48 @@
 const getNotes = require('./notes.js')
 const chalk = require('chalk')
 
-// Take input of the 3rd argv variable from cmd line
-const cmd = process.argv[2]
+//Import yargs package
+const yargs = require('yargs')
 
-//Printing the entire array (For Testing)
-console.log(process.argv)
+// Customize yargs version
+yargs.version('1.1.0')
 
+// Add note
+yargs.command({
+    command : 'add',
+    describe: 'Adds a new note',
+    handler: function (){
+        console.log('Adding a new note')
+    }
+})
 
-// Condition to check if users wants to add a new note
-if(cmd === 'add'){
-    console.log('Adding note!')
-}
+// Remove note
+yargs.command({
+    command : 'remove',
+    describe: 'Removes a note',
+    handler: function (){
+        console.log('Removing a note')
+    }
+})
 
-// Condition to check if users wants to remove a new note
-else if(cmd === 'remove'){
-    console.log('Removing note!')
-}
+// List
+yargs.command({
+    command : 'list',
+    describe: 'Lists all notes',
+    handler: function (){
+        console.log('Listing all notes')
+    }
+})
+
+// Read
+yargs.command({
+    command : 'read',
+    describe: 'Read a note',
+    handler: function (){
+        console.log('Reading a note')
+    }
+})
+
+// console.log(process.argv)
+console.log(yargs.argv)
+
